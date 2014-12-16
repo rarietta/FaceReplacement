b53f349073cc6c4dba765acf7f9a74e2f2a1add0
Ricky Arietta <rickyarietta@gmail.com>
2014-12-16 00:56:25 -0500

    results from final test set included; all requirements seem to be met

--- run_program.m
+++ run_program.m
@@ -1,2 +1,5 @@
-replace_faces('SampleSet/easy/');
-replace_faces('SampleSet/hard/');
+%replace_faces('SampleSet/easy/');
+%replace_faces('SampleSet/hard/');
+replace_faces('TestSet/blending/');
+replace_faces('TestSet/pose/');
+replace_faces('TestSet/more/');
